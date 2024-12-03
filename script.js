7aff6e84bf15fc57a7f17b171d0a873db21aff67
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-12-03 16:27:40 -0300

    feat: create editUser() function to edit user button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const form = document.querySelector(".add-user-form");
 const nameInput = document.getElementById("name");
 const imageUrl = document.getElementById("url-image");
 
+let editMode = null;
+
 function saveUsers(users) {
   localStorage.setItem("users", JSON.stringify(users));
 }
@@ -17,11 +19,13 @@ function addUsers() {
   userList.innerHTML = "";
 
   users.forEach((user) => {
-    const userDiv = document.createElement("div")
-    userDiv.classList.add("user-card")
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("user-card");
+    userDiv.dataset.id = user.id;
+
     userDiv.innerHTML = `
       <div class="user-info">
-        <img scr ="${user.image}" alt="user-image" class="user-avatar">
+        <img src="${user.image}" alt="user-image" class="user-avatar">
         <span class="user-name">${user.name}</span>
       </div>
       <div class="user-actions">
@@ -34,42 +38,63 @@ function addUsers() {
         <button type="submit" class="btn btn-add">+ Adicionar Tarefa</button>
       </form>      
     `;
+
+    const editButton = userDiv.querySelector(".btn-edit");
+        
+    editButton.addEventListener("click", () => editUser(user.id));
+    
     userList.appendChild(userDiv);
   });
 }
 
+function editUser(userId) {
+  const users = loadUsers();
+  const userEdit = users.find((user) => user.id === userId);
+
+  if(userEdit) {
+    nameInput.value = userEdit.name;
+    imageUrl.value = userEdit.image;
+    nameInput.dataset.userId = userEdit.id;
+  }
+}
+
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); //não sei se é necessário, mas vou deixar
+  event.preventDefault();
 
   const userName = nameInput.value.trim();
   const userImage = imageUrl.value.trim();
-  const users = loadUsers();
-  let userId = users.length > 0 ? Math.max(...users.map((user) => user.id)) + 1 : 1;  
-
-  if (userName && userImage) {    
+  let users = loadUsers();
     
-    const newUser = {
-      id: userId,
-      name: userName,
-      image: userImage,
-      afazeres: []
-    };
-        
-    users.push(newUser);
+
+  if(userName && userImage) {    
+    const userIndex = users.findIndex((user) => user.id === parseInt(nameInput.dataset.userId));
+
+    if(userIndex !== -1) {
+      users[userIndex].name = userName;
+      users[userIndex].image =userImage;
+
+      delete nameInput.dataset.userId;
+    } else {
+      let userId = users.length > 0 ? Math.max(...users.map((user) => user.id)) + 1 : 1;
+      const newUser = {
+        id: userId,
+        name: userName,
+        image: userImage,
+        afazeres: []
+      };
+      users.push(newUser);
+    }     
+
     saveUsers(users);
     
     nameInput.value = "";
     imageUrl.value = "";
+    
     addUsers();
-
-    // Debugg, só pra ter certeza que listener funcionou
-    alert("Usuário adicionado com sucesso!");
-  } else {
-    alert("Por favor, preencha todos os campos!");
-  }
+  }    
 });
 
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
   addUsers();
 })
